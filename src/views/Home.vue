1609de902dfb95660157292680369e000ee376ee
Hrijul <hrijulchauhan@gmail.com>
2023-09-21 14:17:40 +0530

    redesign 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
-  
-    <section class="w-[50%] rounded-md mx-auto mt-48">
-      <div class="font-[SerifSemibold] text-[3rem]">A new template to feel smarter</div>
-      <div class="w-3/4 font-[SansLight] text-[1.25rem]">
-        <p>&nbsp;</p>
-        <p>
-          Just making a new template starter repo because I got tired of having to setup vue-router over and over again.
-          Equipped with router, tailwind along with headless ui and heroicons already setup. VictorSerif + LabilGrotesk
-          font provided by <a href="https://www.kometa.xyz/buy/victor-serif/" class="text-orange-700">Kometa.</a>
-        </p>
-      </div>
+  <section class="w-[50%] rounded-md mx-auto mt-48">
+    <div class="font-[SerifSemibold] text-[3em] flex items-center">A new template to feel smarter</div>
+    <div class="w-3/4 font-[SansLight] text-[1.25rem]">
+      <p>&nbsp;</p>
+      <p>
+        Just making a new template starter repo because I got tired of having to setup vue-router over and over again.
+        Equipped with router, tailwind along with headless ui and heroicons already setup. VictorSerif + LabilGrotesk
+        font provided by <a href="https://www.kometa.xyz/buy/victor-serif/" class="text-orange-700">Kometa.</a>
+      </p>
+    </div>
   </section>
-
 </template>
 
 <style>
@@ -38,5 +36,15 @@
 
 .heading__text {
   font-family: "SerifMedium";
+}
+
+.scale {
+  height: var(--heading_display);
+  width: var(--heading_display);
+}
+
+div img {
+  height: 4rem;
+  width: auto;
 }
 </style>
